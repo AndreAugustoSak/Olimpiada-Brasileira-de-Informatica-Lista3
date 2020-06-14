d515d8d4a2afdfa79b5895921ee7dd82e7ab4476
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-06-14 15:20:39 -0300

    Update Questao_04.py

--- Questao_04.py
+++ Questao_04.py
@@ -1,19 +1,18 @@
-# para não usar bibliotecas:
 def soma_horario(string_horario1, string_horario2):
     listah1 = string_horario1.split(':')
     listah2 = string_horario2.split(':')
-    soma = []
     for i in range(0, len(listah1)):
         listah1[i] = int(listah1[i])
         listah2[i] = int(listah2[i])
-        soma.append(listah1[i] + listah2[i])
-    if soma[2] == 60:
-        soma[2] = '00'
-        soma[1] += 1
-    if soma[1] == 60:
-        soma[1] == '00'
-        soma[0] += 1
-    print(soma)
+    segundos_listah1 = (listah1[0]*3600) + (listah1[1]*60) + (listah1[2])
+    segundos_listah2 = (listah2[0]*3600) + (listah2[1]*60) + (listah2[2])
+    soma = segundos_listah1 + segundos_listah2
+    horas = soma // 3600
+    minutos = (soma % 3600) // 60
+    segundos = soma - (horas * 3600) - (minutos * 60)
+    if horas > 23:
+        horas = horas - 24
+    return '{}:{}:{}'.format(str(horas).zfill(2), str(minutos).zfill(2), str(segundos).zfill(2))
 
 
-soma_horario('09:33:47', '02:26:13')
+print(soma_horario('09:33:47', '02:26:13'))
